ae165444511aaa0a883d35012c578fa04985201b
anitajebaraj <anita.tvl@gmail.com>
2013-11-04 19:59:47 -0800

    changing jobs

--- procurement-service/src/main/java/edu/sjsu/cmpe/procurement/StartupJob.java
+++ procurement-service/src/main/java/edu/sjsu/cmpe/procurement/StartupJob.java
@@ -58,37 +58,39 @@ public void doJob(){
 			
 		if(!book.getOrderIsbnList().isEmpty())
 		{
-			//Http POST
-			Client client = Client.create(); 
-		    WebResource webResourceForPost = client.resource("http://54.215.210.214:9000/orders");  
-		    String input = "{\"id\":\"05322\",\"order_book_isbns\":"+book.getOrderIsbnList()+"}";
-		    System.out.println("input=="+input);
-		    ClientResponse responseFromPost = webResourceForPost.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, input);
-		      
-		    System.out.println("Response " + responseFromPost.getEntity(String.class));
-
-		    String responseMsg=responseFromPost.getEntity(String.class);	
+				
   
    // HTTP GET
     //if(book.getOrderIsbnList().get(0)==null)
-    Client clientGet = Client.create(); 
-    System.out.println("client is created");
-	//WebResource webResourceForGet = clientGet.resource("http://54.215.210.214:9000/orders/05322");
-	WebResource webResourceForGet = clientGet.resource("http://ip.jsontest.com/");
 
-	System.out.println("web resource is ok");
-	ClientResponse responseFromGet = webResourceForGet.accept("application/json").get(ClientResponse.class);
-System.out.println("client response is ok");
-	if (responseFromGet.getStatus() != 200) {
-	   throw new RuntimeException("Failed : HTTP error code : "+ responseFromGet.getStatus());
-	}
+		    Client clientGet = Client.create(); 
+		    System.out.println("client is created");
+			WebResource webResourceForGet = clientGet.resource("http://54.215.210.214:9000/orders/05322");
+			//WebResource webResourceForGet = clientGet.resource("http://ip.jsontest.com/");
+		
+			System.out.println("web resource is ok");
+			ClientResponse responseFromGet = webResourceForGet.accept("application/json").get(ClientResponse.class);
+		System.out.println("client response is ok");
+			if (responseFromGet.getStatus() != 200) {
+			   throw new RuntimeException("Failed : HTTP error code : "+ responseFromGet.getStatus());
+			}
+		  
 
 	String output = responseFromGet.getEntity(String.class);
 	System.out.println("Output from Server .... \n");
 	System.out.println(output);
 	
 	book.setResponseFromGet(output);
-	
+	//Http POST
+	Client client = Client.create(); 
+    WebResource webResourceForPost = client.resource("http://54.215.210.214:9000/orders");  
+    String input = "{\"id\":\"05322\",\"order_book_isbns\":"+book.getOrderIsbnList()+"}";
+    System.out.println("input=="+input);
+    ClientResponse responseFromPost = webResourceForPost.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, input);
+      
+    System.out.println("Response " + responseFromPost.getEntity(String.class));
+
+    String responseMsg=responseFromPost.getEntity(String.class);
 	
 	//logic for publishing the output from server to library
 	
